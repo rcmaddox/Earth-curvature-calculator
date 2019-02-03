89402de316eea3b7cc2562c68430cc57520f57ca
rcmaddox <colemaddox@protonmail.com>
2019-02-03 13:20:16 -0500

    main

---
+++ main.py
@@ -0,0 +1,54 @@
+from sys import argv
+import math
+
+script, units, dist = argv
+
+# Earth's radius (km)
+rad = 6371.0
+
+units = units.upper()
+
+# Convert distance to a floating point number
+dist = float(dist)
+
+if units == "MI":
+    # Convert Earth's radius to miles
+    rad = rad / 1.609
+elif units != "KM":
+    print "ERROR: invalid parameter"
+
+# Calculate circumference
+circ = 2.0 * float(math.pi) * rad
+
+# Calculate degrees per km/mi
+a = 360.0 / circ
+
+e = a * dist
+
+# Convert degrees to radians
+j = e * (math.pi / 180)
+
+# Calculate curvature
+curv = rad * (1.0 - math.cos(j))
+
+# Limit decimal places
+if curv > 1000:
+    rnd = 2
+elif curv > 100:
+    rnd = 3
+elif curv > 10:
+    rnd = 4
+else:
+    rnd = 5
+
+curv = round(curv, rnd)
+
+# Print output
+print """
+Distance: %r
+Curvature: %f
+""" % (dist, curv)
+    
+
+
+
